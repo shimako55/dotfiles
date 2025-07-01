0b56ee9e06f1616e5f6f801c532b59e57cbda6ec
shimako55 <rosa.cimalys@gmail.com>
2025-07-02 01:30:25 +0900

    update

--- .config/wezterm/.wezterm.lua
+++ .config/wezterm/.wezterm.lua
@@ -1,5 +1,5 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
@@ -10,33 +10,36 @@ local config = wezterm.config_builder()
 -- config.color_scheme = 'Dracula'
 -- config.color_scheme = "Poimandres"
 --
-local poimandres = require('colors/poimandres').setup {}
+local poimandres = require("colors/poimandres").setup({})
 config.colors = poimandres
-
 
 config.window_decorations = "NONE"
 
 config.font_size = 16.0
-config.font = wezterm.font("MesloLGS NF", {weight="Medium", stretch="Normal", style="Normal"})
+
+config.font = wezterm.font_with_fallback({
+	"HackGen35 Console NF",
+	"MesloLGS NF",
+})
 
 config.audible_bell = "Disabled"
 
 config.keys = {
-  {
-    key = 'd',
-    mods = 'SHIFT|CTRL',
-    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
-  },
-  {
-    key = ']',
-    mods = 'CTRL',
-    action = wezterm.action.PaneSelect
-  },
-  {
-    key = 'n',
-    mods = 'SHIFT|CTRL',
-    action = wezterm.action.ToggleFullScreen,
-  },
+	{
+		key = "d",
+		mods = "SHIFT|CTRL",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "]",
+		mods = "CTRL",
+		action = wezterm.action.PaneSelect,
+	},
+	{
+		key = "n",
+		mods = "SHIFT|CTRL",
+		action = wezterm.action.ToggleFullScreen,
+	},
 }
 
 -- and finally, return the configuration to wezterm
